2cf5104a8f47f1392f9c881a4dc7413e1c9d22c1
Matthew Brady <mojbrady@gmail.com>
2020-05-04 14:56:22 +0200

    i am rich simple app

---
+++ i_am_rich/android/app/src/main/kotlin/org/mojbrady/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.mojbrady.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+var routes = {
+  '/': (context) => IAmRichScreen(),
+};
+
+void main() {
+  runApp(
+    MaterialApp(
+//    routes: routes,
+      home: IAmRichScreen(),
+    ),
+  );
+}
+
+class IAmRichScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade400,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey.shade700,
+        title: Text('I Am Rich'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
