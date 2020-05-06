16a01c5dff19ce66e6c31fb37bfbeb8727b9616e
Matthew Brady <mojbrady@gmail.com>
2020-05-06 18:17:54 +0200

    commit magic 8 ball

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+void main() {
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Magic 8 Ball',
+        home: Magic8BallApp()),
+  );
+}
+
+class Magic8BallApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Center(
+            child: Padding(
+          padding: EdgeInsets.all(16),
+          child: MagicBallWidget(),
+        )));
+  }
+}
+
+class MagicBallWidget extends StatefulWidget {
+  @override
+  _MagicBallWidgetState createState() => _MagicBallWidgetState();
+}
+
+class _MagicBallWidgetState extends State<MagicBallWidget> {
+  int _number = 1;
+  void _shakeBall() {
+    setState(() {
+      int tmp = _number;
+      while (tmp == _number) {
+        _number = Random().nextInt(5) + 1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () => _shakeBall(),
+      child: Image.asset('images/ball$_number.png'),
     );
+  }
+}
